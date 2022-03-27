4f8e7770c0b0f65445a7fef39c8a59939db6966b
Rafael Nascimento <rafael.cnascimento85@gmail.com>
2022-03-27 10:24:43 -0300

    1 Sessão terminada
    Termino da primeira sessão do curso de PHP pela Udemy

---
+++ 1_intro/ex1.php
@@ -0,0 +1,18 @@
+
+
+<?php
+
+/*Este é o primeiro exercício proposto para que se compreenda
+o aspecto básico da linguagem php. 
+- Criar um arquivo php.
+"Foi criado o arquivo ex1.php"
+- O arquivo deve ter a instrução que imprima seu nome na tela.
+"Utilizei a instrução echo seguido do meu nome completo"
+- Execute o arquivo.
+"foi executado o arquivo pelo localhost e constatado que o código
+está corretamente escrito"
+*/  
+
+    echo "Rafael de Carvalho Nascimento";
+
+?>
---
+++ 1_intro/info.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+Arquivo criado com a intenção de utilizar a instrução/função phpinfo()
+*/
+
+    phpinfo()
+
+?>
