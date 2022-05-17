a60c100fd3ec04cf540e30f4e9c4efb7b02800f8
Rafael Nascimento <rafael.cnascimento85@gmail.com>
2022-05-17 00:11:39 -0300

    Finalização da Seção 5

---
+++ 5_expressoes _operadores/29_op_ternario/index.php
@@ -0,0 +1,19 @@
+<?php
+// Onde ? = if e : igual else
+
+//true
+echo 20 > 10 ? "Deu true <br>" : "Deu false <br>";
+
+//false
+echo 20 > 50 ? "Deu true <br>" : "Deu false <br>";
+
+$a = 10;
+$b = 5;
+
+echo $a > $b ? "Deu true <br>" : "Deu false <br>"; // true
+
+echo $a === $b ? "Deu true <br>" : "Deu false <br>"; // false
+
+echo $a === $b && 10 > 5 ? "Deu true <br>" : "Deu false <br>"; // false
+
+?>
---
+++ 5_expressoes _operadores/30_exercício_20/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    $a = 10;
+    $b = 5;
+
+    echo 7 <= 5 ? "Deu true <br>" : "Deu false <br>"; //false
+
+    echo !(7 <= 5) ? "Deu true <br>" : "Deu false <br>"; //true
+
+
+?>
